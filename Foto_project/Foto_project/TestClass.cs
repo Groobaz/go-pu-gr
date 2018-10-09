2e13ee2225e9faa00d1b20030dc3b316cca63d93
DESKTOP-787OTF4\Bartek <bkramek92@gmail.com>
2018-10-09 23:03:02 +0200

    1.Adding new test class.

---
+++ Foto_project/Foto_project/TestClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Foto_project
+{
+    class TestClass
+    {
+        public void testClass()
+        {
+
+        }
+    }
+}
